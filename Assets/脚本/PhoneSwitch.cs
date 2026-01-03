a8916f405406d8195f70daa830b215b72441b89a
Maxyjof <3478124403@qq.com>
2026-01-03 21:38:21 +0800

    添加了手机设备检测的脚本，自动开关手机UI

---
+++ Assets/脚本/PhoneSwitch.cs
@@ -0,0 +1,23 @@
+
+
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PhoneSwitch: MonoBehaviour
+{
+    public GameObject PhoneHUD;
+    
+    private void Awake()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            PhoneHUD.SetActive(true);
+            Screen.orientation = ScreenOrientation.LandscapeRight;
+        }
+        else
+        {
+            PhoneHUD.SetActive(false);
+        }
+    }
+}
